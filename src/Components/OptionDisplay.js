8eb5fe7cdbc79b3a45355803e2cc1bf278d32898
Holden Matthews <holdenmatthews1@gmail.com>
2023-03-14 10:00:40 -0600

    part 3 complete

--- src/Components/OptionDisplay.js
+++ src/Components/OptionDisplay.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectPotentials } from "../redux/slices/potentialCountriesSlice";
 
 const OptionDisplay = () => {
-    return <div className="stack">OptionDisplay</div>;
+    let currentPotentials = useSelector(selectPotentials)
+    console.log(currentPotentials)
+    return <div className="stack">{currentPotentials.map((country) => {
+        return (
+            <h2>{country.name.common}</h2>
+        )
+    })}</div>;
 };
 
 export default OptionDisplay;
